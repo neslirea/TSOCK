05085273b12e012c1eacac86253880ded3456b3d
Martin Mattew <mmartin6@insa-20030>
2022-02-10 10:28:28 +0100

    content

--- tsock_v0.c
+++ tsock_v0.c
@@ -18,6 +18,12 @@ données du réseau */
 /* pour la gestion des erreurs */
 #include <errno.h>
 
+
+// En tant que source :
+// 1 - creation d'un socket (local)
+// 2 - construire @IP à atteindre = SOCKET DISTANT
+// 3 - envoi de données au port PORT
+
 void construire_message(char *message, char motif, int lg) {
     int i;
     for (i=0;i<lg;i++){
@@ -30,18 +36,27 @@ void afficher_message(char *message, int lg){
     printf("message construit : ");
     for (i=0;i<lg;i++){ 
         printf("%c", message[i]);
-        printf("\n");
     } 
+	printf("\n");
 }
 
 void main (int argc, char **argv)
 {
 	int c;
-	extern char *optarg;
-	extern int optind;
-	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
-	int source = -1 ; /* 0=puits, 1=source */
-    int protocole = -1; /* 0 = UDP sinon TCP */
+   	extern char *optarg;
+   	extern int optind;
+  	int port = htons(atoi(argv[argc-1]));
+	char* host = argv[argc-2];
+   	int nb_message = 10; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
+   	int taille_donnee = 30;
+   	int source = -1 ; /* 0=puits, 1=source */
+   	int protocole = -1; /* 0 = UDP sinon TCP */
+
+	// Variables construction socket
+	int domaine = AF_INET;
+	int type;
+	int proto;
+
 	while ((c = getopt(argc, argv, "upn:s")) != -1) {
 		switch (c) {
 		case 'p': // tsock -p [-options] PORT --> exécution en tant que puit sur le port PORT
@@ -66,6 +81,8 @@ void main (int argc, char **argv)
         
         case 'u': // -u : utilise le service du protocole UDP; par défaut, le protocole TCP est utilisé
             protocole = 0;
+			type = SOCK_DGRAM;
+			proto = IPPROTO_UDP;
             break;
 
 		default:
@@ -87,10 +104,10 @@ void main (int argc, char **argv)
     }
     
     if (protocole == 0){
-        printf("le protocole utilise est UDP");
+        printf("le protocole utilise est UDP \n");
     }
     else{
-        printf("le protocole utilise est TCP")
+        printf("le protocole utilise est TCP \n");
     }
 
 	if (nb_message != -1) {
@@ -108,4 +125,46 @@ void main (int argc, char **argv)
 		    printf("nb de tampons à envoyer = infini\n");
         }
 	}
+
+	//// --- CREATION DU SOCKET LOCAL --- ////
+	int sock = socket(domaine, type, proto); // Renvoie -1 SI erreur SINON renvoie une representation interne du socket)
+	// utile socket --> http://manpagesfr.free.fr/man/man2/socket.2.html
+	if(sock == -1){
+		printf("erreur socket (local)");
+		exit(1);
+	}
+
+	printf("sock = %d \n", sock);
+
+	//// --- CONSTRUCTION SOCKET DISTANT + @ --- //// 
+	// déclaration 
+	struct hostent *hp;
+	struct sockaddr_in adr_distant;
+
+	// affectation domaine et port
+	memset((char*)&adr_distant, 0, sizeof(adr_distant));
+	adr_distant.sin_family = AF_INET; // domaine internet
+	adr_distant.sin_port = port; // port port
+
+	// affection @IP 
+	if((hp = gethostbyname(host)) == NULL){
+		printf("erreur gethostbyname\n");
+		exit(1);
+	}
+	memcpy((char*)&(adr_distant.sin_addr.s_addr), hp->h_addr, hp->h_length);
+
+	//// --- EMISSION DU MESSAGE --- //
+	int lg_adr_distant = sizeof(adr_distant);
+	char * message = malloc(sizeof(char)*(taille_donnee+1));
+   	for (int i=0;i<nb_message;i++){
+       	construire_message(message, (char)65+i, taille_donnee);
+		printf("\n");
+		//printf("%s",message);
+       	afficher_message(message, taille_donnee);
+       	//Envoi du message 
+		sendto(sock,message,taille_donnee, 0,(const struct sockaddr *)&adr_distant, lg_adr_distant);
+	   	}
+   	free(message);
+
+
 }
